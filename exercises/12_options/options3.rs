dbbf99a1c44793b0e07573145da7438639376011
Simon Huang <simh@live.ca>
2024-04-21 12:21:47 -0400

    add some progress in options and errors

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -17,5 +15,5 @@ fn main() {
         Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
-    y; // Fix without deleting this line.
+    y; // Fix without deleting this line
 }
--- exercises/13_error_handling/errors2.rs
+++ exercises/13_error_handling/errors2.rs
@@ -19,16 +19,26 @@
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
+
+// pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
+//     let processing_fee = 1;
+//     let cost_per_item = 5;
+
+//     Ok(item_quantity.parse::<i32>()? * cost_per_item + processing_fee)
+// }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    let quantity_result = item_quantity.parse::<i32>();
 
-    Ok(qty * cost_per_item + processing_fee)
+    let quantity = match quantity_result {
+        Ok(q) => q,
+        Err(error) => Err(error),
+    };
+
+    Ok(quantity * cost_per_item + processing_fee)
 }
 
 #[cfg(test)]
